68bb79d5a963e9e749655ba084e1a6c59039126d
ericl0u <loueric11@gmail.com>
2024-09-02 21:53:38 -0400

    Added user tables and its attributes
    Added user tables and its attributes for sequelize

---
+++ migrations/20240903011504-create-user.js
@@ -0,0 +1,78 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      email: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      username: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      nickname: {
+        allowNull:false,
+        type: Sequelize.STRING
+      },
+      sex: {
+        allowNull:false,
+        defaultValue:9,
+        type: Sequelize.TINYINT
+      },
+      company: {
+        type: Sequelize.STRING
+      },
+      introduce: {
+        type: Sequelize.TEXT
+      },
+      role: {
+        allowNull:false,
+        defaultValue:0,
+        type: Sequelize.TINYINT.UNSIGNED
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addIndex(
+      'User', {
+        fields:['email'],
+        unique:true
+      }
+    )
+    await queryInterface.addIndex(
+      'User', {
+        fields:['username'],
+        unique:true
+      }
+    )
+    await queryInterface.addIndex(
+      'User', {
+        fields:['role'],
+      }
+    )
+
+
+
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
